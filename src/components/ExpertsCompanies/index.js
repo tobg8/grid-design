5d876d311d6b7238fa48b55f7a3717df4b4dbaa6
TheoB-png <theo.beloeil.pro@gmail.com>
2021-04-10 17:08:08 +0200

    hover on companies section - vars-scss

--- src/components/ExpertsCompanies/index.js
+++ src/components/ExpertsCompanies/index.js
@@ -26,13 +26,19 @@ const ExpertsCompanies = () => {
                         alt="ok"
                         className="container__company-logo container__company-logo--modifier-virgin"
                     />
+                    <a className="container__hover-circle" href="https://virgin.com" target="_blank" rel="noreferrer">
+                        Virgin
+                    </a>
                 </div>
                 <div className="container__company">
                     <img 
                         src={apple} 
                         alt="ok"
                         className="container__company-logo container__company-logo--modifier-apple"
-                    /> 
+                    />
+                    <a className="container__hover-circle" href="https://apple.com" target="_blank" rel="noreferrer">
+                        Apple
+                    </a>
                 </div>
             </div>
             <div className="container__companies">
@@ -41,14 +47,20 @@ const ExpertsCompanies = () => {
                         src={swoosh} 
                         alt="ok"
                         className="container__company-logo container__company-logo--modifier-swoosh"
-                    /> 
+                    />
+                    <a className="container__hover-circle container__hover-circle--modifier" href="https://nike.com" target="_blank" rel="noreferrer">
+                        Nike
+                    </a>
                 </div>
                 <div className="container__company">
                     <img 
                         src={wolff} 
                         alt="ok"
                         className="container__company-logo container__company-logo--modifier-wolf"
-                    /> 
+                    />
+                    <a className="container__hover-circle container__hover-circle--modifier" href="https://www.wolffolins.com/" target="_blank" rel="noreferrer">
+                        Wolff Olins
+                    </a>
                 </div>
             </div>
             <div className="container__companies">
@@ -57,14 +69,20 @@ const ExpertsCompanies = () => {
                         src="https://upload.wikimedia.org/wikipedia/commons/2/2b/Windows_logo_2012-Black.svg" 
                         alt="ok"
                         className="container__company-logo"
-                    /> 
+                    />
+                    <a className="container__hover-circle container__hover-circle--modifier" href="https://microsoft.com/" target="_blank" rel="noreferrer">
+                        Microsoft
+                    </a>
                 </div>
                 <div className="container__company">
                     <img 
                         src={facebookFull} 
                         alt="ok"
                         className="container__company-logo container__company-logo--modifier-fb"
-                    /> 
+                    />
+                    <a className="container__hover-circle container__hover-circle--modifier" href="https://facebook.com/" target="_blank" rel="noreferrer">
+                        Facebook
+                    </a>
                 </div>
             </div>
             <div className="container__companies">
@@ -73,14 +91,20 @@ const ExpertsCompanies = () => {
                         src={google}
                         alt="ok"
                         className="container__company-logo"
-                    /> 
+                    />
+                    <a className="container__hover-circle container__hover-circle--modifier" href="https://google.com/" target="_blank" rel="noreferrer">
+                        Google
+                    </a>
                 </div>
                 <div className="container__company">
                     <img 
                         src={ustwo} 
                         alt="ok"
                         className="container__company-logo"
-                    /> 
+                    />
+                    <a className="container__hover-circle container__hover-circle--modifier" href="https://ustwo.com/" target="_blank" rel="noreferrer">
+                        Ustwo
+                    </a>
                 </div>
             </div>
             <div className="container__companies">
@@ -90,6 +114,9 @@ const ExpertsCompanies = () => {
                         alt="ok"
                         className="container__company-logo  container__company-logo--modifier-twitch"
                     /> 
+                    <a className="container__hover-circle container__hover-circle--modifier" href="https://twitch.com/" target="_blank" rel="noreferrer">
+                        Twitch
+                    </a>
                 </div>
                 <div className="container__company">
                     <img 
@@ -97,6 +124,10 @@ const ExpertsCompanies = () => {
                         alt="ok"
                         className="container__company-logo container__company-logo--modifier-eth"
                     /> 
+                    <a className="container__hover-circle container__hover-circle--modifier" href="https://ethereum.org/fr/" target="_blank" rel="noreferrer">
+                        Ethereum
+                    </a>
+                    
                 </div>
             </div>
         </div>
