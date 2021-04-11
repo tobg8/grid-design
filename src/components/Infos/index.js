ad736500edffe7fb979bb577408676a32f23b3eb
TheoB-png <theo.beloeil.pro@gmail.com>
2021-04-11 16:11:26 +0200

    animation on scroll on Infos component

--- src/components/Infos/index.js
+++ src/components/Infos/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './styles.scss';
+import GrowElementOnScroll from '../../hooks/GrowElementOnScroll';
 
 const Infos = () => {
+
+    const [circleRef, isVisible] = GrowElementOnScroll ({
+        root: null,
+        rootMargin: "0px",
+        threshold: 1.0
+    });
+    
     return (
         <div className="infos">
-            <h1 className="infos__title"><div className="infos__circle" />What is it</h1>
+            <h1 className="infos__title"><div className={isVisible ? 'infos__circle infos__circle--modifier' : 'infos__circle'} />What is it</h1>
             <p className="infos__paragraphs infos__paragraphs--modifier">An executive programme broken down into 5 intense 2-week modules in London and California</p>
-            <p className="infos__paragraphs">
+            <p className="infos__paragraphs" ref={circleRef}>
                 Forget boring professors and outdated business knowledge. 
                 You will be taught by CEOs, CTOs and Heads of Design from the top businesses, startups and products companies.
             </p>
